665fd49adcf7271d524c2e1815b90f8cc4568c25
yessyprmtsr <yessypermatasari8@gmail.com>
2021-07-17 17:29:12 +0700

    create main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/style.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+  debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Montserrat'
+      ),
       home: HomePage(),
     );
   }
@@ -22,6 +26,111 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Trending Movie", style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                      )
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 9),
+                            margin: EdgeInsets.only(top: 0.5),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(25)),
+                              color: btnPurple
+                            ),
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.add_shopping_cart,
+                                  color: Colors.white,
+                                    size: 15,
+                                  ),
+                                  Text(" Book", style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 13,
+                                  ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 3),
+                            margin: EdgeInsets.symmetric(horizontal: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(bottomRight: Radius.circular(20), bottomLeft: Radius.circular(20)),
+
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              SizedBox(height: 10),
+                Text("Look out movie that trending today", style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15,
+                ),),
+                SizedBox(height: 15,),
+                SingleChildScrollView(
+                  padding: EdgeInsets.only(bottom: 20),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width*0.5,
+                        height: 300,
+                        padding: EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          color: purple,
+                            boxShadow: [BoxShadow(spreadRadius: 0, offset: Offset(0,10), blurRadius: 0, color: purple.withOpacity(0.4))]
+                        ),
+                        child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage("images/p_avengersendgame_19751_e14a0104.jpeg")
+                                    )
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color blue = new Color(0xff50c9ff);
+Color green = new Color(0xff00e582);
+Color black = new Color(0xff4d4d4d);
+Color btnPurple = new Color(0xff4a148c);
+Color purple = new Color(0xff7c4dff);
